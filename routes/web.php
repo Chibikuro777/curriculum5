672705e9a49ec56be7e84b4a9b7cdc288262967c
Chibikuro777 <56582396+Chibikuro777@users.noreply.github.com>
2022-04-05 18:19:12 +0900

    第5章カリキュラム

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create'); # 追記
     Route::get('news', 'Admin\NewsController@index'); # 追記
+    Route::get('news/edit', 'Admin\NewsController@edit'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 Auth::routes();
